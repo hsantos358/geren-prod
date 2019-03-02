4281a38ee8a3b0981b6529d8874390401e3c37b3
henrique.isantos <henrique.isantos@CAS555487W1029.senacsp.edu.br>
2019-03-01 22:09:29 -0300

    Operação i++

--- geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/GerenProd.java
+++ geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/GerenProd.java
@@ -14,6 +14,6 @@ public class GerenProd {
     public static void main(String[] args) {
         System.out.println("Teste Maven");
         int i = 0;
-        System.out.println("Valor de i++: " + i);
+        System.out.println("Valor de i++: " + i++);
     }
 }
